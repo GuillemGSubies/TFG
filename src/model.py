a8be6a6676be2f7ced498fb856955255732cf3a0
guillem.garcia <guillem.garcia@iic.uam.es>
2019-01-09 17:58:01 +0100

    Bidirectional layer + better text generation

--- src/model.py
+++ src/model.py
@@ -2,9 +2,8 @@ import keras.utils as ku
 import numpy as np
 
 from keras.callbacks import EarlyStopping
-from keras.layers import Dense, Flatten
 from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import LSTM
+from keras.layers import CuDNNLSTM, Bidirectional, Dense, Flatten, LSTM
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -32,7 +31,6 @@ class BaseNetwork(BaseEstimator):
         input_sequences = []
         for line in corpus:
             # TODO: Probar con fastText y HashingVectorizer y los demás de text de keras.
-            # Onehot no hace falta si usamos embedding
             token_list = self.tokenizer.texts_to_sequences([line])[0]
             for i in range(1, len(token_list)):
                 input_sequences.append(token_list[: i + 1])
@@ -57,8 +55,8 @@ class BaseNetwork(BaseEstimator):
             token_list = pad_sequences(
                 [token_list], maxlen=self.max_sequence_len - 1, padding="pre"
             )
-            predicted = self.net.predict_classes(token_list, verbose=0)
-
+            predicted = self.net.predict(token_list, verbose=0)[0]
+            predicted = sample(np.log(predicted), 0.5)
             output_word = ""
             for word, index in self.tokenizer.word_index.items():
                 if index == predicted:
@@ -67,36 +65,45 @@ class BaseNetwork(BaseEstimator):
             seed_text += " " + output_word
         return seed_text
 
+    def compile(
+        self,
+        activation="softmax",
+        arch="Baseline",
+        gpu=False,
+        loss="categorical_crossentropy",
+        metrics=[perplexity_raw],
+        optimizer="adam",
+        hidden_lstm=0,
+        lstm_units=32,
+    ):
+
+        self.net = Sequential()
+        self.net.add(
+            Embedding(self.total_words, 64, input_length=self.max_sequence_len - 1)
+        )
+
+        if arch == "Baseline":
+            self.Baseline()
+        elif arch == "LSTM_Embedding":
+            self.LSTM_Embedding(gpu=gpu, hidden_lstm=hidden_lstm, lstm_units=lstm_units)
+        else:
+            raise Exception("Unknown network architecture")
+
+        self.net.add(Dense(self.total_words, activation=activation))
+        self.net.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+        print(self.summary())
+
     def fit(
         self,
         X_train,
         X_test,
         y_train,
         y_test,
-        arch="Baseline",
+        batch_size=16,
         earlystop=None,
         epochs=200,
-        batch_size=None,
-        verbose=2,
-        activation="softmax",
-        optimizer="adam",
-        loss="categorical_crossentropy",
-        metrics=[perplexity_raw],
-        hidden_lstm=1,
+        verbose=1,
     ):
-
-        self.net = Sequential()
-        self.net.add(Embedding(self.total_words, 64, input_length=X_train.shape[1]))
-
-        if arch == "Baseline":
-            self.Baseline(activation=activation)
-        elif arch == "LSTM_Embedding":
-            self.LSTM_Embedding(hidden_lstm=hidden_lstm, activation=activation)
-        else:
-            raise Exception("Unknown network architecture")
-
-        self.net.compile(loss=loss, optimizer=optimizer, metrics=metrics)
-        print(self.net.summary())
 
         if earlystop is True:
             earlystop = [
@@ -107,29 +114,58 @@ class BaseNetwork(BaseEstimator):
         self.net.fit(
             X_train,
             y_train,
-            epochs=epochs,
-            batch_size=None,
-            verbose=verbose,
+            batch_size=batch_size,
             callbacks=earlystop,
+            epochs=epochs,
             validation_data=(X_test, y_test),
+            verbose=verbose,
         )
 
-    def Baseline(self, activation):
+    def Baseline(self):
         """Simple network with an embedding layer and a dense one"""
 
         self.net.add(Flatten())
-        self.net.add(Dense(self.total_words, activation=activation))
 
         return self
 
-    def LSTM_Embedding(self, hidden_lstm, activation):
+    def LSTM_Embedding(self, gpu, hidden_lstm, lstm_units):
         """ LSTM Network """
 
-        for _ in range(hidden_lstm):
+        lstm = CuDNNLSTM if gpu else LSTM
+        return_sequences = False if hidden_lstm == 0 else True
+
+        layer = lstm(
+            lstm_units,
+            input_shape=(self.max_sequence_len,),
+            return_sequences=return_sequences,
+        )
+        self.net.add(Bidirectional(layer, merge_mode="concat"))
+        for _ in range(hidden_lstm - 1):
             self.net.add(
-                LSTM(32, input_shape=(self.max_sequence_len,), return_sequences=True)
+                lstm(
+                    lstm_units,
+                    input_shape=(self.max_sequence_len,),
+                    return_sequences=True,
+                )
             )
-        self.net.add(LSTM(32, input_shape=(self.max_sequence_len,)))
-        self.net.add(Dense(self.total_words, activation=activation))
+        if hidden_lstm >= 1:
+            self.net.add(lstm(lstm_units, input_shape=(self.max_sequence_len,)))
 
         return self
+
+    def summary(self):
+        return self.net.summary()
+
+
+def sample(logprobs, temperature=1.0):
+    """Modifies probabilities with a given temperature, to add creativity
+    Devuelve el indice"""
+    probs = np.exp(logprobs / temperature)
+    normprobs = normalize(probs)
+    return np.argmax(np.random.multinomial(1, normprobs, 1))
+
+
+def normalize(probs):
+    """Normalizes a list of probabilities, so that they sum up to 1"""
+    prob_factor = 1 / sum(probs)
+    return [prob_factor * p for p in probs]
